55a414a07331618ebaa813a7e913998b84ac1f55
Maro de Melo <marodemelo10@gmail.com>
2021-08-24 20:24:45 -0300

    testando as coordenadas

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
-import * as Location from 'expo-location';
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null); 
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
     })();
-  });
-  let text = 'Aguarde o GPS...';
+  }, []);
+
+  let text = "Waiting..";
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
   }
+
   return (
     <View style={styles.container}>
-    <StatusBar hidden={true}/>
-    <Text>{text}</Text>
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-},});
+    marginTop: "15%"
+  },  
+});
